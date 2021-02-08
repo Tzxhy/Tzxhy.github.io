829047aad3c9fe1d40a5fd32a5a5721110cd0fc5
tanzhixuan <tanzhixuan@bytedance.com>
2021-02-08 21:11:19 +0800

    Site updated: 2021-02-08 21:11:18

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -1,1 +1,99 @@
-$(document).ready((function(){!function(){var t=".post-toc",o=$(t),s=".active-current";function e(){$(".post-toc .active-current").removeClass(s.substring(1))}o.on("activate.bs.scrollspy",(function(){var t=$(".post-toc .active").last();e(),t.addClass("active-current"),o.scrollTop(t.offset().top-o.offset().top+o.scrollTop()-o.height()/2)})).on("clear.bs.scrollspy",e),$("body").scrollspy({target:t})}()})),$(document).ready((function(){var t=$("html"),o=200,s=$.isFunction(t.velocity);$(".sidebar-nav li").on("click",(function(){var t=$(this),e="sidebar-nav-active",a="sidebar-panel-active";if(!t.hasClass(e)){var i=$(".sidebar-panel-active"),l=$("."+t.data("target"));s?i.velocity("transition.slideUpOut",o,(function(){l.velocity("stop").velocity("transition.slideDownIn",o).addClass(a)})):i.animate({opacity:0},o,(function(){i.hide(),l.stop().css({opacity:0,display:"block"}).animate({opacity:1},o,(function(){i.removeClass(a),l.addClass(a)}))})),t.siblings().removeClass(e),t.addClass(e)}})),$(".post-toc a").on("click",(function(o){o.preventDefault();var e=NexT.utils.escapeSelector(this.getAttribute("href")),a=$(e).offset().top;s?t.velocity("stop").velocity("scroll",{offset:a+"px",mobileHA:!1}):$("html, body").stop().animate({scrollTop:a},500)}));var e=$(".post-toc-content"),a="post"===CONFIG.sidebar.display||"always"===CONFIG.sidebar.display,i=e.length>0&&e.html().trim().length>0;a&&i&&(CONFIG.motion.enable?NexT.motion.middleWares.sidebar=function(){NexT.utils.displaySidebar()}:NexT.utils.displaySidebar())}));
+/* global NexT: true */
+
+$(document).ready(function () {
+
+  initScrollSpy();
+
+  function initScrollSpy () {
+    var tocSelector = '.post-toc';
+    var $tocElement = $(tocSelector);
+    var activeCurrentSelector = '.active-current';
+
+    $tocElement
+      .on('activate.bs.scrollspy', function () {
+        var $currentActiveElement = $(tocSelector + ' .active').last();
+
+        removeCurrentActiveClass();
+        $currentActiveElement.addClass('active-current');
+
+        // Scrolling to center active TOC element if TOC content is taller then viewport.
+        $tocElement.scrollTop($currentActiveElement.offset().top - $tocElement.offset().top + $tocElement.scrollTop() - ($tocElement.height() / 2));
+      })
+      .on('clear.bs.scrollspy', removeCurrentActiveClass);
+
+    $('body').scrollspy({ target: tocSelector });
+
+    function removeCurrentActiveClass () {
+      $(tocSelector + ' ' + activeCurrentSelector)
+        .removeClass(activeCurrentSelector.substring(1));
+    }
+  }
+
+});
+
+$(document).ready(function () {
+  var html = $('html');
+  var TAB_ANIMATE_DURATION = 200;
+  var hasVelocity = $.isFunction(html.velocity);
+
+  $('.sidebar-nav li').on('click', function () {
+    var item = $(this);
+    var activeTabClassName = 'sidebar-nav-active';
+    var activePanelClassName = 'sidebar-panel-active';
+    if (item.hasClass(activeTabClassName)) {
+      return;
+    }
+
+    var currentTarget = $('.' + activePanelClassName);
+    var target = $('.' + item.data('target'));
+
+    hasVelocity ?
+      currentTarget.velocity('transition.slideUpOut', TAB_ANIMATE_DURATION, function () {
+        target
+          .velocity('stop')
+          .velocity('transition.slideDownIn', TAB_ANIMATE_DURATION)
+          .addClass(activePanelClassName);
+      }) :
+      currentTarget.animate({ opacity: 0 }, TAB_ANIMATE_DURATION, function () {
+        currentTarget.hide();
+        target
+          .stop()
+          .css({'opacity': 0, 'display': 'block'})
+          .animate({ opacity: 1 }, TAB_ANIMATE_DURATION, function () {
+            currentTarget.removeClass(activePanelClassName);
+            target.addClass(activePanelClassName);
+          });
+      });
+
+    item.siblings().removeClass(activeTabClassName);
+    item.addClass(activeTabClassName);
+  });
+
+  // TOC item animation navigate & prevent #item selector in adress bar.
+  $('.post-toc a').on('click', function (e) {
+    e.preventDefault();
+    var targetSelector = NexT.utils.escapeSelector(this.getAttribute('href'));
+    var offset = $(targetSelector).offset().top;
+
+    hasVelocity ?
+      html.velocity('stop').velocity('scroll', {
+        offset: offset  + 'px',
+        mobileHA: false
+      }) :
+      $('html, body').stop().animate({
+        scrollTop: offset
+      }, 500);
+  });
+
+  // Expand sidebar on post detail page by default, when post has a toc.
+  var $tocContent = $('.post-toc-content');
+  var isSidebarCouldDisplay = CONFIG.sidebar.display === 'post' ||
+      CONFIG.sidebar.display === 'always';
+  var hasTOC = $tocContent.length > 0 && $tocContent.html().trim().length > 0;
+  if (isSidebarCouldDisplay && hasTOC) {
+    CONFIG.motion.enable ?
+      (NexT.motion.middleWares.sidebar = function () {
+          NexT.utils.displaySidebar();
+      }) : NexT.utils.displaySidebar();
+  }
+});
